55367d2164d592957d5bfbd2ce974dbf17855672
Slapec <slapec@gmail.com>
2016-08-31 20:47:45 +0200

    Loading exported blender models work

--- src/ts/objects.ts
+++ src/ts/objects.ts
@@ -4,48 +4,30 @@
  * Contains all the 3d object and scenes
  *
  * @author Caiwan
+ * @author Slapec
  */
-
-/// <reference path="../../typings/index.d.ts" />
 
 import * as THREE from 'three';
 
 export let scene = new THREE.Scene();
 
-// export const light0 = new THREE.DirectionalLight(0x66ffff, 0.7);
-// scene.add(light0);
-// light0.position.set(1, 3, 0);
-
-// export const light1 = new THREE.DirectionalLight(0xffffcc, 0.7);
-// scene.add(light1);
-// light1.position.set(-1, 3, 0);
+export const light0 = new THREE.AmbientLight(0xffffff);
+scene.add(light0);
 
 /** Builds up avatar model */
 export class AvatarModel {
-    public avatar: THREE.Object3D;
+    public avatar: THREE.Mesh;
 
     constructor() {
-        let loader = new THREE.ObjectLoader();
+        let loader = new THREE.JSONLoader();
 
-        // let geometry = new THREE.BoxGeometry(1, 1, 1);
-        // let material = new THREE.MeshLambertMaterial({ color: 0x0080ff, overdraw: 0.5 });
-        // let cube = new THREE.Mesh(geometry, material);
-        // this.avatar = cube;
-
-        let scene_: THREE.Scene;
-        loader.load('assets/cube.json', (scene_: THREE.Scene) => {
-            // this.avatar = <THREE.Mesh>scene_.getObjectByName("Cube");
-            this.avatar = scene_;
+        loader.load('assets/cube.json', (geometry, materials) => {
+            this.avatar = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
             this.avatar.scale.set(.5, .5, .5);
-            this.avatar.position.set(0, 0, 0);
-            // this.avatar.material = material;
-
-            console.log("cube", this.avatar);
-        });
-        setInterval(()=>{
             scene.add(this.avatar);
-        },1000);
-        
+            
+            console.log(this.avatar);
+        });
     }
 }
 
